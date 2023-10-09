0023920944d11fecdbed93f7d6c58a8563a3de66
M0fen <carloscaicedo188@gmail.com>
2023-10-09 14:14:58 -0500

    commit to deploy

--- api/index.js
+++ api/index.js
@@ -8,40 +8,41 @@ import cookieParser from 'cookie-parser';
 import path from 'path';
 dotenv.config();
 
-// Conexión a MongoDB
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log('Conectado a Mongodivi');
+    console.log('Connected to MongoDB!');
   })
   .catch((err) => {
     console.log(err);
   });
 
-const __dirname = path.resolve();
+  const __dirname = path.resolve();
 
 const app = express();
 
 app.use(express.json());
+
 app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log('El servidor está escuchando desde el puerto 3k!');
+  console.log('Server is running on port 3000!');
 });
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
-app.use(express.static(path.join(__dirname, '/client/dist ')));
 
-app.get('*',(req, res) => {
-res.send(path.join(__dirname, 'client/dist,index.html'));
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Error interno';
+  const message = err.message || 'Internal Server Error';
   return res.status(statusCode).json({
     success: false,
     statusCode,
