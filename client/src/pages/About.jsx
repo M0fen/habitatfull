e8972535044790c5de149baaa5a8099a5c9ed5ce
M0fen <carloscaicedo188@gmail.com>
2023-10-10 16:13:09 -0500

    about update

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,11 +4,11 @@ export default function About() {
   return (
     <div className='py-20 px-4 max-w-6xl mx-auto'>
       <h1 className='text-3xl font-bold mb-4 text-slate-800'>Sobre HABITAT Propiedad Raíz</h1>
-      <p className='mb-4 text-slate-700'> HABITAT Propiedad Raíz es una agencia inmobiliaria líder que se especializa en ayudar a los clientes a comprar, vender y alquilar propiedades en los barrios más deseables. Nuestro equipo de agentes experimentados se dedica a proporcionar un servicio excepcional y a hacer que el proceso de compra y venta sea lo más fluido posible.</p>
+      <p className='mb-4 text-slate-700'> HABITAT Propiedad Raíz es una agencia inmobiliaria que te ofrece soluciones integrales para comprar, vender o alquilar la propiedad de tus sueños. Contamos con un equipo de agentes profesionales que te brindan una atención personalizada y te acompañan en todo el proceso de negociación.</p>
       <p className='mb-4 text-slate-700'>
-      Nuestra misión es ayudar a nuestros clientes a alcanzar sus objetivos inmobiliarios proporcionando asesoramiento experto, servicio personalizado y un profundo conocimiento del mercado local. Ya sea que estés buscando comprar, vender o alquilar una propiedad, estamos aquí para ayudarte en cada paso del camino.
+      Nuestra misión es satisfacer las necesidades y expectativas de nuestros clientes, ofreciéndoles un servicio de calidad, confianza y seguridad. Queremos que encuentres la propiedad que se adapte a tu estilo de vida, presupuesto y preferencias. Por eso, analizamos el mercado inmobiliario y te asesoramos con honestidad y transparencia..
       </p>
-      <p className='mb-4 text-slate-700'>Nuestro equipo de agentes tiene una gran experiencia y conocimiento en la industria inmobiliaria, y estamos comprometidos a proporcionar el más alto nivel de servicio a nuestros clientes. Creemos que comprar o vender una propiedad debería ser una experiencia emocionante y gratificante, y estamos dedicados a hacer de eso una realidad para cada uno de nuestros clientes..</p>
+      <p className='mb-4 text-slate-700'>Nuestro equipo de agentes tiene una amplia trayectoria y formación en el sector inmobiliario, y se actualiza constantemente para estar al día con las tendencias y oportunidades del mercado. Nos apasiona lo que hacemos y nos esforzamos por lograr la satisfacción de nuestros clientes. Creemos que comprar o vender una propiedad es una decisión importante que debe ser tomada con tranquilidad y seguridad, y estamos dispuestos a ayudarte a hacerla realidad.</p>
     </div>
   )
 }
